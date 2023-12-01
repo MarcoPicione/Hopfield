509c53c7354ce5f4b753585b68e3cb63bc36d013
marcopicione98@gmail.com <marco@LAPTOP-56D5MLAP>
2023-12-01 10:59:27 +0100

    updated

--- Hopfield.py
+++ Hopfield.py
@@ -4,6 +4,7 @@ import numpy as np
 import tkinter as tk
 import random
 import numbers_dict
+import matplotlib.pyplot as plt
 
 # GRAPHICS
 
@@ -51,25 +52,31 @@ def plot_stuff(width, num, initial, active):
 # NETWORK    
 
 class hopfield(object):
-    def __init__(self, size, tau = None, termalization = None, eta = None):
+    def __init__(self, size, dt = None, tau = None, temperature = None, learningRate = None, alpha = None):
         self.size = size
-        if eta is None: eta = 1
-        self.eta = eta
+        if learningRate is None: learningRate = 1
+        self.learningRate = learningRate
         if tau is None: tau = 1
         self.tau = tau
-        if termalization is None: termalization = 0
-        self.termalization = termalization
+        if dt is None: dt = 1
+        self.dt = dt
+        if temperature is None: temperature = 0
+        self.temperature = temperature
+        if alpha is None: alpha = 1
+        self.alpha = alpha
 
         self.weights = np.zeros((size, size))
+        self.patterns = []
         self.state = np.zeros(size)
         self.state_rounded = np.zeros(size)
+        self.collective_variable = []
         
     def add_pattern(self, pattern):
-        self.weights = (self.weights + np.outer(pattern, pattern)) * self.eta
+        self.weights = (self.weights + np.outer(pattern, pattern)) * self.learningRate
+        self.patterns.append(pattern)
         for i in range(self.size):
             self.weights[i][i] = 0
         
-
     def activation(self, i):
         return self.act(np.dot(self.weights[i], self.state))
 
@@ -78,29 +85,37 @@ class hopfield(object):
         self.state = state.astype(np.float64)
         for it in range(iterations):
             for i in range(self.size):
-                self.state[i] =  self.state[i] - ((self.state[i] - self.activation(i) ) / self.tau) + self.termalization
+                self.state[i] =  self.state[i] - ((self.state[i] - self.activation(i)) * self.dt / self.tau) + np.sqrt(2 * self.temperature * self.dt) * np.random.normal()
+            self.collective_variable.append(self.evaluate_collective_variable())
 
         self.state_rounded = np.where(self.state>=0, 1, -1)
 
     def act(self, x):
         return np.tanh(x)
+    
+    def distance(self, a, b):
+        return np.linalg.norm(a-b)
+
+    def evaluate_collective_variable(self):
+        return -1 *np.exp(-self.distance(self.patterns[0], self.state) / self.alpha) + 1 * np.exp(-self.distance(self.patterns[1], self.state) / self.alpha)
+
 
 # MAIN
 
 def main():
 
-    n = 81
-    h = hopfield(n)
+    n = 9
+    h = hopfield(n, temperature=0.15, dt=1/100)
     d = numbers_dict.d
 
-    # h.add_pattern(d['one'+str(n)])
+    h.add_pattern(d['one'+str(n)])
     h.add_pattern(d['two'+str(n)])
     # h.add_pattern(d['three'+str(n)])
     # h.add_pattern(d['four'+str(n)])
     # h.add_pattern(d['five'+str(n)])
     # h.add_pattern(d['six'+str(n)])
     # h.add_pattern(d['seven'+str(n)])
-    h.add_pattern(d['eight'+str(n)])
+    # h.add_pattern(d['eight'+str(n)])
     # h.add_pattern(d['nine'+str(n)])
 
     state = []
@@ -110,10 +125,17 @@ def main():
 
     # state = d['nine81']
 
-    h.update(1000, state)
+    h.update(100000, state)
 
     width = 400
     plot_stuff(width, n, state, h.state_rounded)
+
+    plt.figure()
+    plt.plot(range(len(h.collective_variable)), h.collective_variable)
+    plt.xlabel('Iteration')
+    plt.ylabel('Collective variable')
+    plt.show()
+
 
 if __name__== '__main__':
     main()
